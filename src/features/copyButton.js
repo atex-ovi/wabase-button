ffabe1596aa318e56fa92c4f6e8572bb73b1c40f
Atex Ovi <zhangdingliliang@gmail.com>
2025-11-04 02:34:45 +0330

    Update copyButton.js

--- src/features/copyButton.js
+++ src/features/copyButton.js
@@ -1,11 +1,11 @@
 export async function handleCopyButton(sock, jid) {
   await sock.sendMessage(jid, {
-    text: '📋 Ini contoh Copy Button!',
+    text: '📋 This is an example of a Copy Button!',
     interactiveButtons: [
       {
         name: 'cta_copy',
         buttonParamsJson: JSON.stringify({
-          display_text: 'Salin Link',
+          display_text: 'Copy Link',
           copy_code: 'https://github.com/atex-ovi'
         })
       }
