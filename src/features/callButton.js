d066b71db6fb816f8d42f166bd719eb4b7d60125
Atex Ovi <zhangdingliliang@gmail.com>
2025-11-04 02:33:44 +0330

    Update callButton.js

--- src/features/callButton.js
+++ src/features/callButton.js
@@ -1,11 +1,11 @@
 export async function handleCallButton(sock, jid) {
   await sock.sendMessage(jid, {
-    text: '📞 Ini contoh Call Button!',
+    text: '📞 This is an example of a Call Button!',
     interactiveButtons: [
       {
         name: 'cta_call',
         buttonParamsJson: JSON.stringify({
-          display_text: 'Telepon Owner',
+          display_text: 'Call Owner',
           phone_number: '628xxxxxxx'
         })
       }
